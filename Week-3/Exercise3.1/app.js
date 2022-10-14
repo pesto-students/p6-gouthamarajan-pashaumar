576556d2982de0bc61a1165af181d609730b6b2c
pashaumar <pasha.u.144@gmail.com>
2022-10-14 22:27:24 +0530

    week3 assignment added files and folder for all the exercise completed exercise3.1

---
+++ Week-3/Exercise3.1/app.js
@@ -0,0 +1,24 @@
+const memoize = (fn) => {
+  const cache = {};
+  return function (...args) {
+    const key = args.toString();
+    if (cache[key]) {
+      console.log("cache");
+      return cache[key];
+    } else {
+      const sum = args.reduce((curr, item) => fn(curr, item), 0);
+      cache[key] = sum;
+      console.log("calculation");
+      return sum;
+    }
+  };
+};
+
+const add = (a, b) => a + b;
+
+const memoizeAdd = memoize(add);
+
+console.log(memoizeAdd(100, 100));
+console.log(memoizeAdd(100, 100));
+console.log(memoizeAdd(100, 100, 100));
+console.log(memoizeAdd(100, 100, 100));
